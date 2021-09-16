fd1f33c668cd6454c5ac8b4e49bb933265e9a88d
TN57-BotZ <90815719+TN57-BotZ@users.noreply.github.com>
2021-09-16 12:36:24 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,10 +5,10 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Channel", url="https://t.me/aryan_bots")],
+        [InlineKeyboardButton("Support", url="https://t.me/TN57_BotZSupport")],
         [InlineKeyboardButton(
-            "Report Bugs 😊", url="https://t.me/aryanvikash")]
+            "Leech & Mirror Zone", url="https://t.me/TN57_Leech")]
     ])
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n I'm Simple Bot To Download YouTube Videos & Thumbnail. Worked By [TN57 BotZ](https://t.me/TN57_BotZ)"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
