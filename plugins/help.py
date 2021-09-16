062720464bcb934c72e7b03907ee653e517279ad
TN57-BotZ <90815719+TN57-BotZ@users.noreply.github.com>
2021-09-16 12:42:11 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Currently Only supports Youtube Single  (No playlist) Just Send Youtube Url"
+    helptxt = f"Just Send YouTube Link And Select Desired Format. Then I am send The Content As Video Or File ☺️. Don't Send Live YouTube Link🙅. Any Error contact @TN57_BotZSupport"
     await message.reply_text(helptxt)
