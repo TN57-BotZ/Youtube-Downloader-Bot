7bbc7e753be792d41e8a166e8a6dd7ecbdae151c
TN57-BotZ <90815719+TN57-BotZ@users.noreply.github.com>
2021-09-17 08:43:27 +0530

    Update youtube.py

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -26,7 +26,7 @@ update_channel = Config.UPDATE_CHANNEL
                     [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/TN57_BotZ")]
               ])
             )
-            return:
+        return:
     userLastDownloadTime = user_time.get(message.chat.id)
     try:
         if userLastDownloadTime > datetime.now():
