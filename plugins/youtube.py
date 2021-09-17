ade8f905fe143b5ce17e3a8dca6adf53ece03f64
TN57-BotZ <90815719+TN57-BotZ@users.noreply.github.com>
2021-09-17 07:45:33 +0530

    Update youtube.py

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -12,6 +12,22 @@ ytregex = r"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[
 
 @Client.on_message(Filters.regex(ytregex))
 async def ytdl(_, message):
+update_channel = Config.UPDATE_CHANNEL
+    if update_channel:
+        try:
+            user = await bot.get_chat_member(update_channel, update.chat.id)
+            if user.status == "kicked":
+               await update.reply_text("<b>Sorry You Are B a n n e d...!!! \n \nContact My Dev 👉 @BluVds To Resolve This Problem</b>")
+               return
+        except UserNotParticipant:
+            await update.reply_text(
+                text="**<b>Oh Dear In Order To Use Me Join My Update Channel 🤭</b>**",
+                reply_markup=InlineKeyboardMarkup([
+                    [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/TN57_BotZ")]
+              ])
+            )
+            return
+        else:
     userLastDownloadTime = user_time.get(message.chat.id)
     try:
         if userLastDownloadTime > datetime.now():
