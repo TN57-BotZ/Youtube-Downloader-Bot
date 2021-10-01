48684478c20449cf4e9526f81cd78c71737f0a7a
TN57-BotZ <90815719+TN57-BotZ@users.noreply.github.com>
2021-10-01 13:34:47 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,11 +1,11 @@
 from pyrogram import Client
-import sample_config
+import config
 
 DOWNLOAD_LOCATION = "./Downloads"
-BOT_TOKEN = sample_config.BOT_TOKEN
+BOT_TOKEN = config.BOT_TOKEN
 
 APP_ID = sample_config.APP_ID
-API_HASH = sample_config.API_HASH
+API_HASH = config.API_HASH
 
 
 plugins = dict(
